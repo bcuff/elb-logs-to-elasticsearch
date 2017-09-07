7c1104560c76ee893aeecf62a1ad72131387887f
Brandon Cuff <brandon@scopely.com>
2017-09-06 18:03:24 -0700

    Fixed elasticsearch query serialization

--- src/elbtoes/HttpInfo.cs
+++ src/elbtoes/HttpInfo.cs
@@ -18,6 +18,16 @@ namespace elbtoes
                 .ToDictionary(v => i++.ToString());
         }
 
+        static Dictionary<string, string> GetQuery(string query)
+        {
+            var values = HttpUtility.ParseQueryString(query);
+            var q = from string key in values.Keys
+                    let theValues = values.GetValues(key)
+                    where theValues != null && theValues.Length > 0
+                    select new KeyValuePair<string, string>(key, theValues[0]);
+            return new Dictionary<string, string>(q);
+        }
+
         public static Parser<HttpInfo> Parser = Empty.Or(
             from method in Parsers.NonWhitespaceToken
             from uri in Parse.AnyChar.Until(Parse.WhiteSpace).TryParse(u => new Uri(new string(u.ToArray())), "a valid URI")
@@ -29,7 +39,7 @@ namespace elbtoes
                 host = uri.Host,
                 port = uri.Port.ToString(),
                 path = GetPath(uri.AbsolutePath),
-                query = HttpUtility.ParseQueryString(uri.Query),
+                query = GetQuery(uri.Query),
                 version = version,
             }
         );
@@ -39,7 +49,7 @@ namespace elbtoes
         public string host { get; private set; }
         public string port { get; private set; }
         public Dictionary<string, string> path { get; private set; }
-        public NameValueCollection query { get; private set; }
+        public Dictionary<string, string> query { get; private set; }
         public string version { get; private set; }
     }
 }
